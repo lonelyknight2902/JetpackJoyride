141e071c65807c287dd73d0621fc4aecafbc5290
lonelyknight2902 <truongthinh2902@gmail.com>
2024-06-25 18:04:09 +0700

    feat: Add BulletFlash game object to Player class

--- src/game-objects/Player.ts
+++ src/game-objects/Player.ts
@@ -6,6 +6,7 @@ import {
     RunState,
     TouchDownState,
 } from '../states/player-states'
+import BulletFlash from './BulletFlash'
 import Jetpack from './Jetpack'
 import PlayerBody from './PlayerBody'
 import PlayerHead from './PlayerHead'
@@ -15,6 +16,7 @@ class Player extends Phaser.GameObjects.Container {
     private _playerBody: PlayerBody
     private _playerHead: PlayerHead
     private _jetpack: Jetpack
+    private _bulletFlash:  BulletFlash
     private _scene: Phaser.Scene
     // private _renderTexture: Phaser.GameObjects.RenderTexture
     // private _container: Phaser.GameObjects.Container
@@ -26,9 +28,13 @@ class Player extends Phaser.GameObjects.Container {
         this._playerBody = new PlayerBody(scene, 16, 22, 'player-body')
         this._playerHead = new PlayerHead(scene, 16, 10, 'player-head')
         this._jetpack = new Jetpack(scene, 4, 19, 'jetpack')
+        this._bulletFlash = new BulletFlash(scene, 4, 45, 'bullet-flash')
+        this._bulletFlash.setScale(0.4)
         this.add(this._playerBody)
         this.add(this._playerHead)
         this.add(this._jetpack)
+        this.add(this._bulletFlash)
+        this._bulletFlash.setVisible(false)
         // this._container = new Phaser.GameObjects.Container(scene, x, y, [this._playerBody, this._playerHead])
         // let body = this._container.body as Phaser.Physics.Arcade.Body
         // this._container.setSize(32, 64)
@@ -74,6 +80,10 @@ class Player extends Phaser.GameObjects.Container {
 
     get jetpack(): Jetpack {
         return this._jetpack
+    }
+
+    get bulletFlash(): BulletFlash {
+        return this._bulletFlash
     }
 
     // updatePlayerTexture() {
