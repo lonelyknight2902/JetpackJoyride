f7e29866146cf4ad5a5cc01adfca6a82fb42b094
lonelyknight2902 <truongthinh2902@gmail.com>
2024-06-25 18:04:38 +0700

    refactor: Remove unused code and optimize platform creation in PlayScene

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -1,5 +1,4 @@
 import { Pickup, Player } from '../game-objects'
-import PickUp from '../game-objects/Pickup'
 
 class PlayScene extends Phaser.Scene {
     private _player: Player
@@ -8,37 +7,32 @@ class PlayScene extends Phaser.Scene {
         super('PlayScene')
     }
 
-    preload() {
-        this.load.spritesheet('player-body', 'assets/Characters/Barry/defaultBody.png', {
-            frameWidth: 32,
-            frameHeight: 32,
-        })
-        this.load.spritesheet('player-head', 'assets/Characters/Barry/defaultHead.png', {
-            frameWidth: 32,
-            frameHeight: 32,
-        })
-        this.load.spritesheet('jetpack', 'assets/Characters/Jetpacks/jetpackDefault.png', {
-            frameWidth: 32,
-            frameHeight: 44,
-        })
-        this.load.spritesheet('pickup', 'assets/Pickup/pickup_TVOS.png', {
-            frameWidth: 128,
-            frameHeight: 128,
-        })
-    }
-
     create() {
         // const renderTexture = this.add.renderTexture(0, 0, 32, 32)
         // renderTexture.saveTexture('player')
-        this._player = new Player(this, 200, 0)
-        this._player.setScale(3)
+        const hallway1 = this.add.image(920, 768 / 2, 'hallway1')
+        hallway1.setOrigin(0, 0.5)
+        const background = this.add.image(-100, 768 / 2, 'titleBackground1')
+        background.setOrigin(0, 0.5)
+        const background2 = this.add.image(1024 - 228, 768 / 2, 'titleBackground2')
+        background2.setOrigin(0, 0.5)
+        this._player = new Player(this, 500, 200)
+        this._player.setScale(2)
         const pickup = new Pickup(this, 200, 200)
         // this.physics.add.existing(this._player)
         // player.play('touchdown')
         this.cursors = this.input.keyboard?.createCursorKeys()
         const platforms = this.physics.add.staticGroup()
-        platforms.create(0, 600, 'ground').setScale(20, 1).refreshBody()
+        platforms.create(1365 / 2, 660, 'ground').setScale(50, 1).refreshBody()
+        platforms.create(1365 / 2, 100, 'ground').setScale(50, 1).refreshBody()
+        platforms.setVisible(false)
+        // this.physics.add.collider(this._player, platforms)
         this.physics.add.collider(this._player, platforms)
+        this.physics.add.collider(this._player, pickup, (player, pickup) => {
+            pickup.destroy()
+        })
+        // this.cameras.main.startFollow(this._player)
+        // this.cameras.
     }
 
     update() {
