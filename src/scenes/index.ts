09d65462419c73e02869d74d49a9f34d483975e9
lonelyknight2902 <truongthinh2902@gmail.com>
2024-06-25 18:04:24 +0700

    feat: Add PreloadScene and LoadingScene to scene exports

--- src/scenes/index.ts
+++ src/scenes/index.ts
@@ -1,2 +1,4 @@
 export { default as TestScene } from './TestScene'
 export { default as PlayScene } from './PlayScene'
+export { default as PreloadScene } from './PreloadScene'
+export { default as LoadingScene } from './LoadingScene'
