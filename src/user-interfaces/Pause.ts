dd1df0ba363ea77d81dcb9f84cb71732724a7105
lonelyknight2902 <truongthinh2902@gmail.com>
2024-07-05 11:16:21 +0700

    refactor: Add Pause and Result user interfaces

--- src/user-interfaces/Pause.ts
+++ src/user-interfaces/Pause.ts
@@ -1,11 +1,27 @@
+import { PlayScene } from "../scenes"
+import Button from "./Button"
+
 class Pause extends Phaser.GameObjects.Container {
     private pauseText: Phaser.GameObjects.BitmapText
+    private continueButton: Button
+    private homeButton: Button
     // private screen: Phaser.GameObjects.Sprite
-    constructor(scene: Phaser.Scene, x: number, y: number) {
+    constructor(scene: PlayScene, x: number, y: number) {
         super(scene, x, y)
         this.pauseText = scene.add.bitmapText(0, -200, 'NewAthleticM54White', 'PAUSE', 64)
         this.pauseText.setOrigin(0.5, 0)
+        this.continueButton = new Button(scene, 0, 0, 'Continue', () => {
+            console.log('Continue')
+            scene.stateMachine.transition('play')
+        })
+        this.homeButton = new Button(scene, 0, 100, 'Home', () => {
+            console.log('Home')
+            scene.stateMachine.transition('start')
+        })
+
         this.add(this.pauseText)
+        this.add(this.continueButton)
+        this.add(this.homeButton)
         scene.add.existing(this)
     }
 }
--- src/user-interfaces/Result.ts
+++ src/user-interfaces/Result.ts
@@ -1,10 +1,13 @@
 import { ScoreManager } from '../manager'
+import { PlayScene } from '../scenes'
+import Button from './Button'
 
 class Result extends Phaser.GameObjects.Container {
     private distanceText: Phaser.GameObjects.BitmapText
-    private coinText: Phaser.GameObjects.Text
+    private coinText: Phaser.GameObjects.BitmapText
     private scoreManager: ScoreManager
-    constructor(scene: Phaser.Scene, x: number, y: number) {
+    private nextButton: Button
+    constructor(scene: PlayScene, x: number, y: number) {
         super(scene, x, y)
         this.scoreManager = ScoreManager.getInstance()
         // const youFlewText = scene.add.text(0, 0, 'YOU FLEW', {
@@ -26,19 +29,21 @@ class Result extends Phaser.GameObjects.Container {
             72
         )
         const andCollectedText = scene.add.bitmapText(0, 140, 'NewAthleticM54GrayInverted', 'AND COLLECTED', 32)
-        this.coinText = scene.add.text(0, 180, 'Coins: 0', {
-            fontFamily: 'Arial',
-            fontSize: 32,
-            color: '#ffffff',
-        })
+        this.coinText = scene.add.bitmapText(0, 200, 'NewAthleticM54Gold', `${this.scoreManager.getCoin()}`, 32)
         youFlewText.setOrigin(0.5)
         this.distanceText.setOrigin(0.5)
         andCollectedText.setOrigin(0.5)
         this.coinText.setOrigin(0.5)
+        this.nextButton = new Button(scene, 0, 300, 'Next', () => {
+            console.log('Next')
+            // scene.scene.start('play')
+            scene.stateMachine.transition('start')
+        })
         this.add(youFlewText)
         this.add(this.distanceText)
         this.add(andCollectedText)
         this.add(this.coinText)
+        this.add(this.nextButton)
         scene.add.existing(this)
     }
 
